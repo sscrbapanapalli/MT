6e65ab23b001cc265026f63babe18f3963575ff2
SSC.BAMMU <SSC.BAMMU@INCHEDTDM4173.INCHESSC.COM>
2018-05-09 11:12:33 +0530

    added employee details class

---
+++ src/main/java/com/cmacgm/cdrserver/model/EmployeeDetails.java
@@ -0,0 +1,139 @@
+package com.cmacgm.cdrserver.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name="Employee_Details")
+public class EmployeeDetails {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name="emp_id", length=100)
+	private String empId;	
+		
+	@Column(name="emp_name", length=100)
+	private String empName;
+	
+	@Column(name="rm_id", length=100)
+	private String rmId;	
+		
+	@Column(name="rm_name", length=100)
+	private String rmName;
+
+	@Column(name="is_rm")
+	private Boolean isRm=false;
+	
+	
+	@CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="created_date")
+    private Date createdDate;
+    
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="updated_date")
+    private Date updatedDate;
+    
+    @Column(name="created_by",length = 100)    
+    private String createdBy;
+    
+    @Column(name="updated_by",length = 100)
+    private String updatedBy;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getRmId() {
+		return rmId;
+	}
+
+	public void setRmId(String rmId) {
+		this.rmId = rmId;
+	}
+
+	public String getRmName() {
+		return rmName;
+	}
+
+	public void setRmName(String rmName) {
+		this.rmName = rmName;
+	}
+
+	public Boolean getIsRm() {
+		return isRm;
+	}
+
+	public void setIsRm(Boolean isRm) {
+		this.isRm = isRm;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	
+    
+
+}
