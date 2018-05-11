77b5450d87a6183286b6d0ca867200d2184f2685
SSC.BAMMU <SSC.BAMMU@INCHEDTDM4173.INCHESSC.COM>
2018-05-11 20:52:47 +0530

    changes

--- src/main/resources/static/app/js/appConfig.js
+++ src/main/resources/static/app/js/appConfig.js
@@ -2,7 +2,9 @@
  var config=angular.module("appConfigApp", [])
 .constant("appConstants",{
 	//serverUrl : "http://10.13.44.33:8080/MyTime",
-	serverUrl : "http://10.13.56.64:8080",
+	//serverUrl : "http://10.13.44.80:8080/MyTime",
+	//serverUrl : "http://10.13.56.64:8080",
+	serverUrl : "http://localhost:8080/MyTime",
 
 }).run(function($http, $window) {	
 	$http.defaults.headers.common['userToken'] =$window.sessionStorage["userToken"]?$window.sessionStorage["userToken"]: null;
