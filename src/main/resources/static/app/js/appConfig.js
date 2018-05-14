0f8d77e3464e412f60a08bd7412c35897497d378
Viuha <ssc.vsvinchuramamoor@cma-cgm.com>
2018-05-14 11:35:22 +0530

    Updated config

--- src/main/resources/static/app/js/appConfig.js
+++ src/main/resources/static/app/js/appConfig.js
@@ -1,10 +1,10 @@
  
  var config=angular.module("appConfigApp", [])
 .constant("appConstants",{
-	serverUrl : "http://10.13.44.33:8080/MyTime",
+	//serverUrl : "http://10.13.44.33:8080/MyTime",
 	//serverUrl : "http://10.13.44.80:8080/MyTime",
 	//serverUrl : "http://10.13.56.64:8080",
-	//serverUrl : "http://localhost:8080",
+	serverUrl : "http://localhost:8080",
 
 }).run(function($http, $window) {	
 	$http.defaults.headers.common['userToken'] =$window.sessionStorage["userToken"]?$window.sessionStorage["userToken"]: null;
