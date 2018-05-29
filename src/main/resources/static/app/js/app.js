d1bef34d42fc16a9ee3bbfc0dce35d9774663964
SSC.BAMMU <SSC.BAMMU@INCHEDTDM4173.INCHESSC.COM>
2018-05-29 11:52:30 +0530

    changes for adding slno

--- src/main/resources/static/app/js/app.js
+++ src/main/resources/static/app/js/app.js
@@ -58,59 +58,6 @@ angular.module('myTimeApp').config(
 
 				} ]);
 
-angular.module('myTimeApp').service('anchorSmoothScroll', function(){
-    
-    this.scrollTo = function(eID) {
-
-        // This scrolling function 
-        // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
-        
-        var startY = currentYPosition();
-        var stopY = elmYPosition(eID);
-        var distance = stopY > startY ? stopY - startY : startY - stopY;
-        if (distance < 100) {
-            scrollTo(0, stopY); return;
-        }
-        var speed = Math.round(distance / 100);
-        if (speed >= 20) speed = 20;
-        var step = Math.round(distance / 25);
-        var leapY = stopY > startY ? startY + step : startY - step;
-        var timer = 0;
-        if (stopY > startY) {
-            for ( var i=startY; i<stopY; i+=step ) {
-                setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
-            } return;
-        }
-        for ( var i=startY; i>stopY; i-=step ) {
-            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
-        }
-        
-        function currentYPosition() {
-            // Firefox, Chrome, Opera, Safari
-            if (self.pageYOffset) return self.pageYOffset;
-            // Internet Explorer 6 - standards mode
-            if (document.documentElement && document.documentElement.scrollTop)
-                return document.documentElement.scrollTop;
-            // Internet Explorer 6, 7 and 8
-            if (document.body.scrollTop) return document.body.scrollTop;
-            return 0;
-        }
-        
-        function elmYPosition(eID) {
-            var elm = document.getElementById(eID);
-            var y = elm.offsetTop;
-            var node = elm;
-            while (node.offsetParent && node.offsetParent != document.body) {
-                node = node.offsetParent;
-                y += node.offsetTop;
-            } return y;
-        }
-
-    };
-    
-});
 
 
 angular.module('myTimeApp').controller(
@@ -1312,9 +1259,9 @@ angular.module('myTimeApp').controller(
 				'$window',
 				'$q',
 				'$http',
-				'appConstants','userService','globalServices','$stateParams','$location','$filter','anchorSmoothScroll',
+				'appConstants','userService','globalServices','$stateParams','$location','$filter',
 				function($scope, $state, $rootScope, $window, $q,
-						$http,appConstants,userService,globalServices,$stateParams,$location,$filter,anchorSmoothScroll) {
+						$http,appConstants,userService,globalServices,$stateParams,$location,$filter) {
 					$scope.homepageContent = "settings dashboard page";
 					$scope.allEmployeeDetails=[];	
 					$scope.employeeCreate="false";
@@ -1466,7 +1413,9 @@ angular.module('myTimeApp').controller(
 								   }
 							}
 							// call $anchorScroll()
-						      anchorSmoothScroll.scrollTo('top');
+						     // anchorSmoothScroll.scrollTo('top');
+							    var $target = $('html,body');
+                               $target.animate({scrollTop: $target.height()}, 500);
 						}
 						
 						
