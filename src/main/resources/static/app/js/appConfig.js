f53f807bb3490a4a189451f7423cc8ff769c3db8
SSC.RBAPANAPALLI <SSC.RBAPANAPALLI@INCHEDTDM4172.INCHESSC.COM>
2018-06-07 11:25:32 +0530

    local db

--- src/main/resources/static/app/js/appConfig.js
+++ src/main/resources/static/app/js/appConfig.js
@@ -4,8 +4,8 @@
 	//serverUrl : "http://10.13.44.33:8080/MyTime",
 	//serverUrl : "http://10.13.44.80:8080/MyTime",
 	//serverUrl : "http://10.13.56.64:8080",
-	serverUrl : "http://localhost:8080",
-	localServer:true,
+	serverUrl : "http://10.13.56.184:8080",
+	localServer:false,
 	windowsUNUrl:"http://10.13.44.33:8080/windowsUN/Testing",
 	userTimeReportUrl:"http://10.13.56.23/ReportServer/Pages/ReportViewer.aspx?%2fSSC%2ftest%2fUserProductivity&rs%3aCommand=Render"
 
