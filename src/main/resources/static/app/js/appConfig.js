c07ace3975ac6bc5da97dc8c5739c92f1d008225
SSC.RBAPANAPALLI <SSC.RBAPANAPALLI@INCHEDTDM4172.INCHESSC.COM>
2018-06-08 11:25:14 +0530

    changed db to mytimedev

--- src/main/resources/static/app/js/appConfig.js
+++ src/main/resources/static/app/js/appConfig.js
@@ -2,9 +2,9 @@
  var config=angular.module("appConfigApp", [])
 .constant("appConstants",{
 	//serverUrl : "http://10.13.44.33:8080/MyTime",
-	//serverUrl : "http://10.13.44.80:8080/MyTime",
+	serverUrl : "http://10.13.44.80:8080/MyTimeDev",
 	//serverUrl : "http://10.13.56.64:8080",
-	serverUrl : "http://10.13.56.184:8080",
+	//serverUrl : "http://10.13.56.184:8080",
 	localServer:false,
 	windowsUNUrl:"http://10.13.44.33:8080/windowsUN/Testing",
 	userTimeReportUrl:"http://10.13.56.23/ReportServer/Pages/ReportViewer.aspx?%2fSSC%2ftest%2fUserProductivity&rs%3aCommand=Render"
